7965d0d8e9db1fe430907f73b2f1eab8f7976bab
Jonty Leslie <jontyleslie@gmail.com>
2025-10-17 05:51:43 +0000

    macos test --lib + clippy

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # left in but needs to be looked at again if necessary
 
 TIBERIUS_TEST_CONNECTION_STRING='server=tcp:localhost,1433;user=SA;password=<YourStrong@Passw0rd>;TrustServerCertificate=true' \
