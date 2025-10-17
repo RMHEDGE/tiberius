c0a8a735e8f82c7e07b5e92e8c571bbd82c30bda
Jonty Leslie <jontyleslie@gmail.com>
2025-10-17 06:08:16 +0000

    macos test --lib + clippy

--- run_tests.sh
+++
@@ -1,4 +0,0 @@
-# left in but needs to be looked at again if necessary
-
-TIBERIUS_TEST_CONNECTION_STRING='server=tcp:localhost,1433;user=SA;password=<YourStrong@Passw0rd>;TrustServerCertificate=true' \
-    cargo test
